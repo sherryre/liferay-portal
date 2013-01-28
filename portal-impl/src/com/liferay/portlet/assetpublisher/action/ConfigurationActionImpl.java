b6a80eddbb21067eee0dddaa78f6acf6956f4d0d
sherryre <sherry.yang@liferay.com>
2013-01-28 13:54:17 -0800

    LPS-32132 rename method -- no logic changes

--- portal-impl/src/com/liferay/portlet/assetpublisher/action/ConfigurationActionImpl.java
+++ portal-impl/src/com/liferay/portlet/assetpublisher/action/ConfigurationActionImpl.java
@@ -109,7 +109,7 @@ public class ConfigurationActionImpl extends DefaultConfigurationAction {
 					removeScope(actionRequest, preferences);
 				}
 				else if (cmd.equals("select-scope")) {
-					setScopes(actionRequest, preferences);
+					setScope(actionRequest, preferences);
 				}
 				else if (cmd.equals("selection-style")) {
 					setSelectionStyle(actionRequest, preferences);
@@ -351,7 +351,7 @@ public class ConfigurationActionImpl extends DefaultConfigurationAction {
 		preferences.setValues("assetEntryXml", newEntries);
 	}
 
-	protected void setScopes(
+	protected void setScope(
 			ActionRequest actionRequest, PortletPreferences preferences)
 		throws Exception {
 
