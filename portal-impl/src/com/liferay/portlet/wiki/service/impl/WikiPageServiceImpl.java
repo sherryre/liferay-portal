2a8cab034abdb2e006a13e5da70aec1587c2aa58
sherryre <sherry.yang@liferay.com>
2013-02-25 15:03:46 -0800

    LPS-33148 Source formatting

--- portal-impl/src/com/liferay/portlet/wiki/service/impl/WikiPageServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/wiki/service/impl/WikiPageServiceImpl.java
@@ -317,7 +317,7 @@ public class WikiPageServiceImpl extends WikiPageServiceBaseImpl {
 		throws PortalException, SystemException {
 
 		WikiNodePermission.check(
-				getPermissionChecker(), nodeId, ActionKeys.VIEW);
+			getPermissionChecker(), nodeId, ActionKeys.VIEW);
 
 		WikiNode node = wikiNodePersistence.findByPrimaryKey(nodeId);
 
@@ -684,7 +684,8 @@ public class WikiPageServiceImpl extends WikiPageServiceBaseImpl {
 					}
 					else {
 						value = getPageDiff(
-							companyId, attachmentURLPrefix, latestPage, page, locale);
+							companyId, attachmentURLPrefix, latestPage, page,
+							locale);
 					}
 
 					syndContent.setValue(value);
